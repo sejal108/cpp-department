c82f2b518365dbbe9b8626cc2a6f5e5f9d279746
Sejal <sejalsheth25@gmail.com>
2025-05-27 16:22:54 +0530

    added

---
+++ Assignment/assignment5.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+class book
+{
+    private:
+    int bookno;
+    string booktitle;
+    float price;
+    totalcost(int n)
+    public:
+    input()
+    {
+        cout << "enter book name" <<endl;
+        cin >> booktitle;
+        cout<<"enter book number"<<endl;
+        cin>>bookno;
+        cout<<"enter book price"<<endl;
+        cin>>price;
+    }
+    
+
+
+}
